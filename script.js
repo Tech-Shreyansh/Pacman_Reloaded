b520e199d8e565edcf4d0f944a8310c99431782a
Dhruv Goyal <15goyaldhruv@gmail.com>
2022-10-10 23:39:57 +0530

    Fixed the Map display bug

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // GAME VARIABLES AND CONSTANTS
 let frames = 0;
 let pos = 130;
+
+let po1 = 414;
+let f = 0;
 let direction = 1;
-let score=0;
+let score = 0;
+let d;
+
 
 // MAP DESIGN (29 X 30)
 const squares = [];
@@ -28,7 +33,7 @@ const coordinates = [
     1, 1, 1, 0, 2, 0, 1, 0, 1, 0, 2, -1, 2, 7, 7, 7, 7, 2, -1, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1,
     1, 1, 1, 0, 2, 0, 1, 0, 1, 0, 0, -1, 2, -1, -1, -1, -1, 2, -1, 2, 0, 0, 0, 0, 0, 0, 1, 1, 1,
     -1, -1, 1, 0, 2, 0, 1, 0, 1, 0, 2, -1, 2, -1, -1, -1, -1, 2, -1, 2, 1, 1, 1, 1, 1, 0, 1, -1, -1,
-    -1, -1, 1, 0, 2, 0, 1, 0, 0, 0, 2, -1, 2, 3, -1, -1, 4, 2, -1, 2, 2, 2, 2, 1, 1, 0, 1, -1, -1,
+    -1, -1, 1, 0, 2, 0, 1, 0, 0, 0, 2, -1, 2, 3, -1, -1, 4, 2, -1, 2, 2, 2, 2, 2, 1, 0, 1, -1, -1,
     -1, -1, 1, 0, 2, 0, 1, 1, 1, 0, 2, -1, 5, 2, 3, 4, 2, 6, -1, 2, 1, 1, 1, 1, 1, 0, 1, -1, -1,
     -1, -1, 1, 0, 2, 0, 0, 0, 0, 0, 2, -1, 1, 5, 2, 2, 6, 1, -1, 2, 0, 0, 0, 0, 0, 0, 1, -1, -1,
     -1, -1, 1, 0, 2, 2, 2, 2, 2, 0, 2, -1, 1, 1, 5, 6, 1, 1, -1, 2, 2, 2, 2, 2, 2, 0, 1, -1, -1,
@@ -110,11 +115,11 @@ function pacman() {
             square.className = "";
             square.classList.add("pacman-down");
         }
-        else if (direction == 4){
+        else if (direction == 4) {
             square.className = "";
             square.classList.add("pacman-left");
         }
-        
+
     }
     else if (frames % 60 == 0) {
         square.className = "";
@@ -125,13 +130,12 @@ function pacman() {
     // square.classList.add("pacman-right");
 }
 
-function scoreCount(){
+function scoreCount() {
     const scoreDisplay = document.getElementById("score");
-    if(squares[pos].classList.contains("food") || squares[pos].classList.contains("food2")) score+=10;
-    else if(squares[pos].classList.contains("power-pellet")) score+=200;
-    scoreDisplay.innerHTML="SCORE: " + score;
+    if (squares[pos].classList.contains("food") || squares[pos].classList.contains("food2")) score += 10;
+    else if (squares[pos].classList.contains("power-pellet")) score += 200;
+    scoreDisplay.innerHTML = "SCORE: " + score;
 }
-
 function updatePacman() {
     document.body.onkeydown = function (e) {
         if (e.key == "ArrowLeft") {
@@ -186,15 +190,234 @@ function updatePacman() {
     }
 }
 
-// Loop
+
+//GHOST
+
+function ghost() {
+    const square = squares[po1];
+    square.className = "";
+    square.classList.add("ghost1");
+}
+
+function updateghost() {
+    if (d < 0.25) {
+        if (frames % 10 == 0) {
+            if (coordinates[po1 + 1] == 1 || coordinates[po1 + 1] == 2) { }
+            else if (po1 == 433) {
+                if (squares[po1 + 1].classList.contains("blank")) {
+                    if (f == 0) {
+                        po1 = 406;
+                        squares[433].className = "";
+                        squares[433].className = "blank";
+                        f = 0;
+                    }
+                    else {
+                        po1 = 406;
+                        squares[433].className = "";
+                        squares[433].className = "food";
+                        f = 0;
+                    }
+                }
+                else {
+                    if (f == 0) {
+                        po1 = 406;
+                        squares[433].className = "";
+                        squares[433].className = "blank";
+                        f = 1;
+                    }
+                    else {
+                        po1 = 406;
+                        squares[433].className = "";
+                        squares[433].className = "food";
+                        f = 1;
+                    }
+                }
+            }
+            else {
+                if (squares[po1 + 1].classList.contains("blank")) {
+                    if (f == 0) {
+                        po1++;
+                        squares[po1 - 1].className = "";
+                        squares[po1 - 1].className = "blank";
+                        f = 0;
+                    }
+                    else {
+                        po1++;
+                        squares[po1 - 1].className = "";
+                        squares[po1 - 1].className = "food";
+                        f = 0;
+                    }
+                }
+                else if (coordinates[po1 + 1] == 0) {
+                    if (f == 0) {
+                        po1++;
+                        squares[po1 - 1].className = "";
+                        squares[po1 - 1].className = "blank";
+                        f = 1;
+                    }
+                    else {
+                        po1++;
+                        squares[po1 - 1].className = "";
+                        squares[po1 - 1].className = "food";
+                        f = 1;
+                    }
+                }
+            }
+        }
+    }
+    else if (d < 0.5) {
+        if (frames % 10 == 0) {
+            if (coordinates[po1 - 1] == 1 || coordinates[po1 - 1] == 2) { }
+            else if (po1 == 407) {
+                if (squares[po1 - 1].classList.contains("blank")) {
+                    if (f == 0) {
+                        po1 = 433;
+                        squares[407].className = "";
+                        squares[407].className = "blank";
+                        f = 0;
+                    }
+                    else {
+                        po1 = 433;
+                        squares[407].className = "";
+                        squares[407].className = "food";
+                        f = 0;
+                    }
+                }
+                else {
+                    if (f == 0) {
+                        po1 = 407;
+                        squares[407].className = "";
+                        squares[407].className = "blank";
+                        f = 1;
+                    }
+                    else {
+                        po1 = 407;
+                        squares[407].className = "";
+                        squares[407].className = "food";
+                        f = 1;
+                    }
+                }
+            }
+            else {
+                if (squares[po1 - 1].classList.contains("blank")) {
+                    if (f == 0) {
+                        po1--;
+                        squares[po1 + 1].className = "";
+                        squares[po1 + 1].className = "blank";
+                        f = 0;
+                    }
+                    else {
+                        po1--;
+                        squares[po1 + 1].className = "";
+                        squares[po1 + 1].className = "food";
+                        f = 0;
+                    }
+                }
+                else if (coordinates[po1 - 1] == 0) {
+                    if (f == 0) {
+                        po1--;
+                        squares[po1 + 1].className = "";
+                        squares[po1 + 1].className = "blank";
+                        f = 1;
+                    }
+                    else {
+                        po1--;
+                        squares[po1 + 1].className = "";
+                        squares[po1 + 1].className = "food";
+                        f = 1;
+                    }
+                }
+            }
+        }
+    }
+    else if (d < 0.75) {
+        if (frames % 10 == 0) {
+            if (coordinates[po1 - 29] == 1 || coordinates[po1 - 29] == 2) { }
+            else {
+                if (squares[po1 - 29].classList.contains("blank")) {
+                    if (f == 0) {
+                        po1 -= 29;
+                        squares[po1 + 29].className = "";
+                        squares[po1 + 29].className = "blank";
+                        f = 0;
+                    }
+                    else {
+                        po1 -= 29;
+                        squares[po1 + 29].className = "";
+                        squares[po1 + 29].className = "food";
+                        f = 0;
+                    }
+                }
+                else if (coordinates[po1 - 29] == 0) {
+                    if (f == 0) {
+                        po1 -= 29;
+                        squares[po1 + 29].className = "";
+                        squares[po1 + 29].className = "blank";
+                        f = 1;
+                    }
+                    else {
+                        po1 -= 29;
+                        squares[po1 + 29].className = "";
+                        squares[po1 + 29].className = "food";
+                        f = 1;
+                    }
+                }
+            }
+        }
+    }
+    else if (d < 1) {
+        if (frames % 10 == 0) {
+            if (coordinates[po1 + 29] == 1 || coordinates[po1 + 29] == 2) { }
+            else {
+                if (squares[po1 + 29].classList.contains("blank")) {
+                    if (f == 0) {
+                        po1 += 29;
+                        squares[po1 - 29].className = "";
+                        squares[po1 - 29].className = "blank";
+                        f = 0;
+                    }
+                    else {
+                        po1 += 29;
+                        squares[po1 - 29].className = "";
+                        squares[po1 - 29].className = "food";
+                        f = 0;
+                    }
+                }
+                else if (coordinates[po1 + 29] == 0) {
+                    if (f == 0) {
+                        po1 += 29;
+                        squares[po1 - 29].className = "";
+                        squares[po1 - 29].className = "blank";
+                        f = 1;
+                    }
+                    else {
+                        po1 += 29;
+                        squares[po1 - 29].className = "";
+                        squares[po1 - 29].className = "food";
+                        f = 1;
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 drawMap();
 function loop() {
-    frames++;
-    updatePacman();
-    pacman();
-    food();
-    window.requestAnimationFrame(loop);
+    if (po1 == pos) {
+        alert("game over");
+    }
+    else {
+        frames++;
+        updatePacman();
+        pacman();
+        ghost();
+        updateghost();
+        food();
+        window.requestAnimationFrame(loop);
+        d = Math.random();
+        console.log(d);
+    }
 }
-// pacman();
 loop();
